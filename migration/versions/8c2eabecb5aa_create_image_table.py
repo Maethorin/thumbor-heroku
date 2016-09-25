fa4860fbc585689ca3e439ae4c2cb1cfcbc771e2
Márcio Duarte <maethorin@gmail.com>
2016-09-25 13:53:31 -0300

    Using Postgres as Storage

---
+++ migration/versions/8c2eabecb5aa_create_image_table.py
@@ -0,0 +1,31 @@
+"""create image table
+
+Revision ID: 8c2eabecb5aa
+Revises: 
+Create Date: 2016-09-24 15:23:22.663732
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8c2eabecb5aa'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'images',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('path', sa.String(), unique=True, nullable=False),
+        sa.Column('data', sa.Text(), nullable=False),
+        sa.Column('detector', sa.Text(), nullable=True),
+        sa.Column('crypto', sa.Text(), nullable=True)
+    )
+
+
+def downgrade():
+    op.drop_table('account')
